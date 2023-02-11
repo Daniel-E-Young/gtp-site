d6b4c7d7cfae58c061afd35b8d55d686221859d3
Daniel Young <109361146+Daniel-E-Young@users.noreply.github.com>
2023-02-11 06:27:10 +0100

    commit

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ function validateForm(event) {
     if (!name) {
         errorMessage += "Name is required. ";
       }
+
+    if (!email) {
+        errorMessage += "Email is required. ";
+      } else if (!isValidEmail(email)) {
+        errorMessage += "Email is invalid. ";
+      }
+    
+    if (!message) {
+        errorMessage += "Message is required. ";
+      }
+
+      if (errorMessage) {
+        alert(errorMessage);
+      } else {
+        alert("Form submitted successfully!");
+      }
+    }
+    
+    function isValidEmail(email) {
+      const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(String(email).toLowerCase());
+    }
