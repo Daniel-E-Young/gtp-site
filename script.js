ba893e48abc6faeb3dc6469c3b929ed9e9275811
Daniel Young <109361146+Daniel-E-Young@users.noreply.github.com>
2023-02-11 06:25:34 +0100

    commit

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ form.addEventListener("submit", validateForm);
 
 function validateForm(event) {
     event.preventDefault();
-    
+
+    const name = document.querySelector("#name").value;
+    const email = document.querySelector("#email").value;
+    const message = document.querySelector("#message").value;
+    let errorMessage = "";
